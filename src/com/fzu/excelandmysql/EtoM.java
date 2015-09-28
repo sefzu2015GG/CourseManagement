f561edeaf7637548c2a8bffcfec7abf60999bb56
MyVolcano <756154017@qq.com>
2015-09-29 00:42:25 +0800

    from java project to web project

---
+++ src/com/fzu/excelandmysql/EtoM.java
@@ -0,0 +1,166 @@
+package com.fzu.excelandmysql;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.WorkbookSettings;
+import jxl.read.biff.BiffException;
+
+import com.fzu.sqlutils.ChoseSqlUtils;
+import com.fzu.sqlutils.CourseBean;
+import com.fzu.sqlutils.CourseSqlUtils;
+
+public class EtoM {
+
+	/**
+	 * 单元测试
+	 * @param argg0
+	 */
+	public static void main(String[] argg0) {
+		System.out.println("test1");
+		String path = "data/计算机 1 - 副本.xls";
+		String tname = "test1";
+		EtoM etom = new EtoM(path, tname);
+	}
+
+	private String filePath = null;
+	private String tablename = null;
+	private static CourseSqlUtils courseSqlUtils = null;
+	private static ChoseSqlUtils choseSqlUtils = null;
+	
+	/**
+	 * 
+	 * @param filePath excel表格的路径，由调用者提供
+	 * @param tablename 数据库建表表名，以excel表为名
+	 */
+	public EtoM(String filePath, String tablename) {
+		this.filePath = filePath;
+		this.tablename = tablename;
+		courseSqlUtils = new CourseSqlUtils(tablename);
+		choseSqlUtils = new ChoseSqlUtils(tablename);
+		List<CourseBean> data = getExcelFile();
+		outpr(data);
+		courseSqlUtils.insert(data);
+	}
+
+	public static CourseSqlUtils getCourseSqlUtils( String tablename) {
+		if(courseSqlUtils==null){
+			courseSqlUtils = new CourseSqlUtils(tablename);
+		}
+		return courseSqlUtils;
+	}
+	
+	public static ChoseSqlUtils getChoseSqlUtils(String tablename) {
+		if(choseSqlUtils==null){
+			choseSqlUtils = new ChoseSqlUtils(tablename);
+		}
+		return choseSqlUtils;
+	}
+	
+	public void outpr(List<CourseBean> list) {
+
+		for (int i = 0; i < list.size(); i++) {
+			CourseBean bean = list.get(i);
+			System.out.println(bean.toString());
+			System.out.println();
+		}
+
+	}
+
+	private List<CourseBean> getExcelFile() {
+		List<CourseBean> list = new ArrayList<CourseBean>();
+		FileInputStream fileInputStream = null;
+		try {
+			fileInputStream = new FileInputStream(filePath);
+
+			WorkbookSettings workbookSettings = new WorkbookSettings();
+			workbookSettings.setEncoding("utf-8");
+//			workbookSettings.setEncoding("gbk");
+
+			Workbook workbook = Workbook.getWorkbook(fileInputStream);
+
+			int sheetNum = workbook.getNumberOfSheets();
+
+			Sheet sheet = workbook.getSheet(0);
+
+			int row = sheet.getRows();
+			int col = sheet.getColumns();
+			System.out.println("row = " + row + ", col = " + col);
+
+			for (int i = 0; i < row; i++) {
+				CourseBean bean = new CourseBean();
+
+				bean.setCoursenum("031202" + i);
+
+				bean.setGrade(sheet.getCell(0, i).getContents()
+						.replaceAll(String.valueOf((char) 160), ""));
+
+				bean.setMajor(sheet.getCell(1, i).getContents()
+						.replaceAll(String.valueOf((char) 160), ""));
+
+				String peoplenum = sheet.getCell(2, i).getContents()
+						.replaceAll(String.valueOf((char) 160), "");
+				if (peoplenum == "") {
+					peoplenum = "0";
+				}
+				bean.setPeoplenum(Integer.parseInt(peoplenum));
+
+				bean.setCoursename(sheet.getCell(3, i).getContents()
+						.replaceAll(String.valueOf((char) 160), ""));
+
+				bean.setType(sheet.getCell(4, i).getContents()
+						.replaceAll(String.valueOf((char) 160), ""));
+
+				String credit = sheet.getCell(5, i).getContents()
+						.replaceAll(String.valueOf((char) 160), "");
+				if (credit == "") {
+					credit = "0";
+				}
+				bean.setCredit(Float.parseFloat(credit));
+
+				String period = sheet.getCell(6, i).getContents()
+						.replaceAll(String.valueOf((char) 160), "");
+				if (period == "") {
+					period = "0";
+				}
+				bean.setPeriod(Integer.parseInt(period));
+
+				String testperiod = sheet.getCell(7, i).getContents()
+						.replaceAll(String.valueOf((char) 160), "");
+				if (testperiod == "") {
+					testperiod = "0";
+				}
+				bean.setTestperiod(Integer.parseInt(testperiod));
+
+				String fuckcomputerperiod = sheet.getCell(8, i).getContents()
+						.replaceAll(String.valueOf((char) 160), "");
+				if (fuckcomputerperiod == "") {
+					fuckcomputerperiod = "0";
+				}
+				bean.setFuckcomputerperiod(Integer.parseInt(fuckcomputerperiod));
+				list.add(bean);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (BiffException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (fileInputStream != null) {
+				try {
+					fileInputStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			fileInputStream = null;
+		}
+		return list;
+	}
+}
---
+++ src/com/fzu/sqlutils/SqlHelper.java
@@ -0,0 +1,313 @@
+package com.fzu.sqlutils;
+
+import java.io.FileInputStream;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class SqlHelper {
+
+	private static Connection connection = null;
+	private static PreparedStatement preparedStatement = null;
+	// private static ResultSet;
+	private static ResultSet resultSet = null;
+	private static CallableStatement callableStatement = null;
+	private static String driver = "";
+	private static String url = "";
+	private static String uerName = "";
+	private static String passWord = "";
+
+	private static Properties properties = null;
+	private static FileInputStream fileInputStream = null;
+
+	public static Connection getCon() {
+
+		return connection;
+	}
+
+	public static PreparedStatement getPreparedStatement() {
+		return preparedStatement;
+	}
+
+	public static ResultSet getResultSet() {
+		return resultSet;
+	}
+
+	public static CallableStatement getCallableStatement() {
+		return callableStatement;
+	}
+
+	/**
+	 *初始化Mysql连接
+	 */
+	static {
+
+		try {
+			fileInputStream = new FileInputStream("file.properties");
+			properties = new Properties();	
+			properties.load(fileInputStream);
+			driver = properties.getProperty("driver");
+			url = properties.getProperty("url");
+			uerName = properties.getProperty("username");
+			passWord = properties.getProperty("password");
+
+			Class.forName(driver);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			if (fileInputStream != null) {
+				try {
+					fileInputStream.close();
+				} catch (Exception e2) {
+					// TODO: handle exception
+				}
+			}
+			fileInputStream = null;
+		}
+
+	}
+
+	public static Connection getConnection() {
+		try {
+			connection = DriverManager.getConnection(url, uerName, passWord);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return connection;
+
+	}
+
+	/**
+	 * 多条sql处理
+	 * @param sql
+	 * @param parmameters
+	 */
+	public static void excuteUpdateMultiParams(String[] sql,
+			Object[][] parmameters) {
+		try {
+			connection = getConnection();
+			connection.setAutoCommit(false);
+
+			for (int i = 0; i < sql.length; i++) {
+				preparedStatement = connection.prepareStatement(sql[i]);
+				for (int j = 0; j < parmameters[i].length; j++) {
+					preparedStatement.setObject(j + 1, parmameters[i][j]);
+				}
+				preparedStatement.executeUpdate();
+			}
+			connection.commit();
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+
+			try {
+				connection.rollback();
+			} catch (Exception e2) {
+				// TODO: handle exception
+				e2.printStackTrace();
+			}
+			// throw new RuntimeErrorException(null, e.getMessage());
+		} finally {
+			close();
+			// close(resultSet, preparedStatement, connection);
+		}
+	}
+
+	/**
+	 * 单条sql多参数处理
+	 * @param sql
+	 * @param parmameters
+	 */
+	public static void excuteUpdateMultiParams(String sql,
+			Object[][] parmameters) {
+		try {
+			connection = getConnection();
+			connection.setAutoCommit(false);
+
+			for (int i = 0; i < parmameters.length; i++) {
+				preparedStatement = connection.prepareStatement(sql);
+				for (int j = 0; j < parmameters[i].length; j++) {
+					preparedStatement.setObject(j + 1, parmameters[i][j]);
+				}
+				preparedStatement.executeUpdate();
+			}
+			connection.commit();
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+
+			try {
+				connection.rollback();
+			} catch (Exception e2) {
+				// TODO: handle exception
+				e2.printStackTrace();
+			}
+			// throw new RuntimeErrorException(null, e.getMessage());
+		} finally {
+			close();
+			// close(resultSet, preparedStatement, connection);
+		}
+	}
+
+	/**
+	 * update,insert,delete等
+	 * @param sql
+	 * @param parameters
+	 */
+	public static void escuteUpdate(String sql, Object[] parameters) {
+
+		try {
+			connection = getConnection();
+			preparedStatement = connection.prepareStatement(sql);
+
+			if (parameters != null) {
+				for (int i = 0; i < parameters.length; i++) {
+					preparedStatement.setObject(i + 1, parameters[i]);
+				}
+			}
+			preparedStatement.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+			// TODO: handle exception
+		} finally {
+			close();
+			// close(resultSet, preparedStatement, connection);
+		}
+
+	}
+
+	public static ResultSet executeQuery(String sql, Object[] parameters) {
+		try {
+			connection = getConnection();
+			preparedStatement = connection.prepareStatement(sql);
+			if (parameters != null) {
+				for (int i = 0; i < parameters.length; i++) {
+					preparedStatement.setObject(i + 1, parameters[i]);
+				}
+			}
+			resultSet = preparedStatement.executeQuery();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+
+		}
+		return resultSet;
+
+	}
+
+	/**
+	 * 储存过程
+	 * @param sql
+	 * @param parameters
+	 */
+
+	public static void callProc(String sql, Object[] parameters) {
+		try {
+			connection = getConnection();
+			callableStatement = connection.prepareCall(sql);
+			// ��ֵ
+			if (parameters != null) {
+				for (int i = 0; i < parameters.length; i++)
+					callableStatement.setObject(i + 1, parameters[i]);
+			}
+			callableStatement.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e.getMessage());
+		} finally {
+			// �ر���Դ
+			closeproc();
+			// close(resultSet, callableStatement, connection);
+		}
+	}
+
+	/**
+	 * @param sql
+	 * @param inparameters
+	 * @return
+	 */
+	public static ResultSet callProcInput(String sql, Object[] inparameters) {
+		try {
+			connection = getConnection();
+			callableStatement = connection.prepareCall(sql);
+			if (inparameters != null)
+				for (int i = 0; i < inparameters.length; i++)
+					callableStatement.setObject(i + 1, inparameters[i]);
+			callableStatement.execute();
+			resultSet = callableStatement.executeQuery();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e.getMessage());
+		} finally {
+			//在由该方法调用者在使用完result之后关闭
+		}
+		return resultSet;
+	}
+
+	/**
+	 * @param sql
+	 * @param outparameters
+	 * @return
+	 */
+	public static ResultSet callProcOutput(String sql, Integer[] outparameters) {
+		try {
+			connection = getConnection();
+			callableStatement = connection.prepareCall(sql);
+			// ��out����ֵ
+			if (outparameters != null)
+				for (int i = 0; i < outparameters.length; i++)
+					callableStatement.registerOutParameter(i + 1,
+							outparameters[i]);
+			callableStatement.execute();
+			resultSet = callableStatement.executeQuery();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e.getMessage());
+		} finally {
+
+		}
+		return resultSet;
+	}
+
+	public static void close() {
+		close(resultSet, preparedStatement, connection);
+	}
+
+	public static void closeproc() {
+		close(resultSet, callableStatement, connection);
+	}
+
+	public static void close(ResultSet rs, Statement ps, Connection conn) {
+		if (rs != null)
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		rs = null;
+		if (ps != null)
+			try {
+				ps.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		ps = null;
+		if (conn != null)
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		conn = null;
+	}
+}
